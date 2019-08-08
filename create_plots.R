a3028b59d7341d10522dfd7be7715723c5cc6207
Trevor Tombe <ttombe@ucalgary.ca>
2019-08-08 11:48:59 -0600

    update to 2018

--- create_plots.R
+++ create_plots.R
@@ -1,92 +1,98 @@
 # Some setup, loads packages and set theme files
-source("setup.R")
+source("core.R")
 
 # Load relevant data
-empdata<-getCANSIM("3830033")
-pop<-getCANSIM("0510001")
+empdata<-getTABLE("36100480")
+pop<-getTABLE("17100005")
+
+# Remove the Industry Codes
+empdata<-empdata %>%
+  mutate(industry_code=str_match(Industry,"\\[(.*?)\\]")[,2],
+         Industry=ifelse(regexpr(" \\[",Industry)>1,
+                         substr(Industry,1,regexpr(" \\[",Industry)-1),Industry))
 
 # Employment by Level of Government
 plotdata<-empdata %>%
-  filter(GEO=="Alberta" & ESTIMATES=="Total number of jobs" &
-           NAICS %in% c("Government educational services",
+  filter(GEO=="Alberta" & Labour.productivity.and.related.measures=="Total number of jobs" &
+           Industry %in% c("Government educational services",
                         "Government health services",
                         "Federal government services (excluding defence)",
                         "Defence services",
                         "Provincial and territorial government services",
                         "Municipal government services",
                         "Aboriginal government services")) %>%
-  mutate(NAICS=replace(NAICS,NAICS=="Government educational services","Education"),
-         NAICS=replace(NAICS,NAICS=="Government health services","Health"),
-         NAICS=replace(NAICS,NAICS=="Federal government services (excluding defence)","Feds, Non-Defense"),
-         NAICS=replace(NAICS,NAICS=="Defence services","Feds, Defense"),
-         NAICS=replace(NAICS,NAICS=="Provincial and territorial government services","Provincial Govt")
-         ,NAICS=replace(NAICS,NAICS=="Municipal government services","Municipal Govt"),
-         NAICS=replace(NAICS,NAICS=="Aboriginal government services","Aboriginal Govt")) %>%
+  mutate(Industry=replace(Industry,Industry=="Government educational services","Education"),
+         Industry=replace(Industry,Industry=="Government health services","Health"),
+         Industry=replace(Industry,Industry=="Federal government services (excluding defence)","Feds, Non-Defense"),
+         Industry=replace(Industry,Industry=="Defence services","Feds, Defense"),
+         Industry=replace(Industry,Industry=="Provincial and territorial government services","Provincial Govt"),
+         Industry=replace(Industry,Industry=="Municipal government services","Municipal Govt"),
+         Industry=replace(Industry,Industry=="Aboriginal government services","Aboriginal Govt")) %>%
   filter(Ref_Date>=2014) %>%
-  group_by(NAICS) %>%
+  group_by(Industry) %>%
   mutate(change=Value-Value[1])
-ggplot(plotdata,aes(Ref_Date,change,group=NAICS,color=NAICS))+
+ggplot(plotdata,aes(Ref_Date,change,group=Industry,color=Industry))+
   geom_line(size=2) +
   geom_hline(yintercept = 0,size=1)+
-  geom_point(data=filter(plotdata,Ref_Date==2016),size=3,show.legend = F)+
+  geom_point(data=filter(plotdata,Ref_Date==2018),size=3,show.legend = F)+
   mytheme+
   scale_colour_brewer(name="",palette="Set1") +
   scale_y_continuous(breaks=pretty_breaks(n=6),label=comma) +
-  scale_x_continuous(breaks=seq(2014,2016)) +
+  scale_x_continuous(breaks=seq(2014,2018)) +
   labs(x="",y="Number of Jobs",
        title="Change in Alberta Public Employment, by Govt Sector",
-       subtitle="",
-       caption="Source: CANSIM 383-0033. Graph by @trevortombe")
-ggsave("plot.png",width=7,height=4,dpi=200)
+       subtitle="Source: Own calculations from Statistics Canada data table 36-10-0480.",
+       caption="Graph by @trevortombe")
+ggsave("plot.png",width=8,height=4,dpi=200)
 
 ## As above, but share of population in 2016
 plotdata<-empdata %>%
-  filter(ESTIMATES=="Total number of jobs" &
-           NAICS %in% c("Government educational services",
+  filter(Labour.productivity.and.related.measures=="Total number of jobs" &
+           Industry %in% c("Government educational services",
                         "Government health services",
                         "Federal government services (excluding defence)",
                         "Defence services",
                         "Provincial and territorial government services",
                         "Municipal government services",
                         "Aboriginal government services")) %>%
-  mutate(NAICS=replace(NAICS,NAICS=="Government educational services","Education"),
-         NAICS=replace(NAICS,NAICS=="Government health services","Health"),
-         NAICS=replace(NAICS,NAICS=="Federal government services (excluding defence)","Feds, Non-Defense"),
-         NAICS=replace(NAICS,NAICS=="Defence services","Feds, Defense"),
-         NAICS=replace(NAICS,NAICS=="Provincial and territorial government services","Provincial Govt")
-         ,NAICS=replace(NAICS,NAICS=="Municipal government services","Municipal Govt"),
-         NAICS=replace(NAICS,NAICS=="Aboriginal government services","Aboriginal Govt")) %>%
+  mutate(Industry=replace(Industry,Industry=="Government educational services","Education"),
+         Industry=replace(Industry,Industry=="Government health services","Health"),
+         Industry=replace(Industry,Industry=="Federal government services (excluding defence)","Feds, Non-Defense"),
+         Industry=replace(Industry,Industry=="Defence services","Feds, Defense"),
+         Industry=replace(Industry,Industry=="Provincial and territorial government services","Provincial Govt"),
+         Industry=replace(Industry,Industry=="Municipal government services","Municipal Govt"),
+         Industry=replace(Industry,Industry=="Aboriginal government services","Aboriginal Govt")) %>%
   filter(Ref_Date>=2014) %>%
-  group_by(NAICS) %>%
+  group_by(Industry) %>%
   mutate(change=Value-Value[1])
 pop2<-pop %>%
-  filter(SEX=="Both sexes" & AGE=="All ages" & Ref_Date==2016) %>%
+  filter(Sex=="Both sexes" & Age.group=="All ages" & Ref_Date==max(Ref_Date)) %>%
   rename(population=Value) %>%
   select(Ref_Date,GEO,population)
 plotdata2<-plotdata %>%
-  filter(Ref_Date==2016) %>%
+  filter(Ref_Date==max(Ref_Date)) %>%
   left_join(pop2,by=c("Ref_Date","GEO")) %>%
   mutate(share=Value/population) %>%
   left_join(provnames,by="GEO") %>%
-  filter(short!="NA" & short!="CAN") %>%
-  select(Ref_Date,short,NAICS,Value,population,share)
-ggplot(plotdata2,aes(short,share,group=NAICS,fill=NAICS))+
-  geom_bar(stat="identity",position="stack",color="black",alpha=0.7)+
+  filter(short!="NA" & short!="CAN" & !short %in% c("YT","NT","NU")) %>%
+  select(Ref_Date,short,Industry,Value,population,share)
+ggplot(plotdata2,aes(short,share,group=Industry,fill=Industry))+
+  geom_col(position="stack",color="white")+
   geom_hline(yintercept=0,size=1)+
   mythemebar+
   scale_fill_brewer(name="",palette = "Set1")+
   scale_y_continuous(breaks=pretty_breaks(n=6),label=percent) +
   labs(x="",y="Per Cent",
-       title="Public Employment as % of Population, by Govt Sector (2016)",
-       subtitle="",
-       caption="Source: CANSIM 383-0033 and 051-0001. Graph by @trevortombe")
-ggsave("plot2.png",width=7,height=4,dpi=200)
+       title="Public Employment as % of Population, by Govt Sector (2018)",
+       subtitle="Source: Own calculations from Statistics Canada data table CANSIM 36-10-0480 and 17-10-0005.",
+       caption="Graph by @trevortombe")
+ggsave("plot2.png",width=8,height=4,dpi=200)
 
 ## AS ABOVE, BUT SHARE OF COMPENSATION
 plotdata<-empdata %>%
-  filter(Ref_Date==2016,
-         ESTIMATES=="Total compensation for all jobs (thousands of dollars)" &
-           NAICS %in% c("Government educational services",
+  filter(Ref_Date==max(Ref_Date),
+         Labour.productivity.and.related.measures=="Total compensation for all jobs" &
+           Industry %in% c("Government educational services",
                         "Government health services",
                         "Federal government services (excluding defence)",
                         "Defence services",
@@ -94,25 +100,25 @@ plotdata<-empdata %>%
                         "Municipal government services",
                         "Aboriginal government services",
                         "All industries")) %>%
-  mutate(NAICS=replace(NAICS,NAICS=="Government educational services","Education"),
-         NAICS=replace(NAICS,NAICS=="Government health services","Health"),
-         NAICS=replace(NAICS,NAICS=="Federal government services (excluding defence)","Feds, Non-Defense"),
-         NAICS=replace(NAICS,NAICS=="Defence services","Feds, Defense"),
-         NAICS=replace(NAICS,NAICS=="Provincial and territorial government services","Provincial Govt"),
-         NAICS=replace(NAICS,NAICS=="Municipal government services","Municipal Govt"),
-         NAICS=replace(NAICS,NAICS=="Aboriginal government services","Aboriginal Govt")) %>%
+  mutate(Industry=replace(Industry,Industry=="Government educational services","Education"),
+         Industry=replace(Industry,Industry=="Government health services","Health"),
+         Industry=replace(Industry,Industry=="Federal government services (excluding defence)","Feds, Non-Defense"),
+         Industry=replace(Industry,Industry=="Defence services","Feds, Defense"),
+         Industry=replace(Industry,Industry=="Provincial and territorial government services","Provincial Govt"),
+         Industry=replace(Industry,Industry=="Municipal government services","Municipal Govt"),
+         Industry=replace(Industry,Industry=="Aboriginal government services","Aboriginal Govt")) %>%
   group_by(GEO) %>%
-  mutate(share=Value/max(Value*(NAICS=="All industries"))) %>%
+  mutate(share=Value/max(Value*(Industry=="All industries"))) %>%
   left_join(provnames,by="GEO") %>%
-  filter(NAICS!="All industries" & short!="NA" & short!="CAN")
-ggplot(plotdata,aes(short,share,group=NAICS,fill=NAICS))+
-  geom_bar(stat="identity",position="stack",color="black",alpha=0.7)+
+  filter(Industry!="All industries" & short!="NA" & short!="CAN"& !short %in% c("YT","NT","NU"))
+ggplot(plotdata,aes(short,share,group=Industry,fill=Industry))+
+  geom_col(position="stack",color="white")+
   geom_hline(yintercept=0,size=1)+
   mythemebar+
   scale_fill_brewer(name="",palette = "Set1")+
   scale_y_continuous(breaks=pretty_breaks(n=6),label=percent) +
   labs(x="",y="Per Cent",
-       title="Public Sector Share of Labour Compensation, by Govt Sector (2016)",
-       subtitle="",
-       caption="Source: CANSIM 383-0033. Graph by @trevortombe")
-ggsave("plot3.png",width=7,height=4,dpi=200)
+       title="Public Sector Share of Labour Compensation, by Govt Sector (2018)",
+       subtitle="Source: Own calculations from Statistics Canada data table CANSIM 36-10-0480",
+       caption="Graph by @trevortombe")
+ggsave("plot3.png",width=8,height=4,dpi=200)
